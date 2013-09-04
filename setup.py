9886e0daf4320f5667e27f095550dc67964cb1e5
Alfredo Deza <alfredo@deza.pe>
2013-09-04 08:30:33 -0400

    create a setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import re
+
+module_file = open("remoto/__init__.py").read()
+metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", module_file))
+long_description = open('README.rst').read()
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'remoto',
+    description = 'Execute remote commands or processes.',
+    packages = find_packages(),
+    author = 'Alfredo Deza',
+    author_email = 'contact [at] deza.pe',
+    install_requires = ['execnet'],
+    version = metadata['version'],
+    url = 'http://github.com/alfredodeza/remoto',
+    license = "MIT",
+    zip_safe = False,
+    keywords = "remote, commands, unix, ssh, socket, execute, terminal",
+    long_description = long_description,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+    ]
+)
