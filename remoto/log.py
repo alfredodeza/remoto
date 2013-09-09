57b19c56b8be8c8131cc3d98cb9f30da3398412b
Alfredo Deza <alfredo@deza.pe>
2013-09-09 08:55:58 -0400

    create a reporting helper for logging channel info

---
+++ remoto/log.py
@@ -0,0 +1,11 @@
+
+
+def reporting(conn, result):
+    log_map = {'debug': conn.logger.debug, 'error': conn.logger.error}
+    while True:
+        try:
+            received = result.receive()
+            level_received, message = received.items()[0]
+            log_map[level_received](message.strip('\n'))
+        except EOFError:
+            break
