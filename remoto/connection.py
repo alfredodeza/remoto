ed27375be84edf57baf23586f7e0bfbf6174b7cb
Alfredo Deza <alfredo@deza.pe>
2013-09-17 10:37:40 -0400

    if the host is local do not do ssh

--- remoto/connection.py
+++ remoto/connection.py
@@ -1,3 +1,4 @@
+import socket
 from .lib import execnet
 
 
@@ -9,9 +10,14 @@ class Connection(object):
 
     def __init__(self, hostname, logger=None, sudo=False):
         self.hostname = hostname
-        self.gateway = execnet.makegateway('ssh=%s' % hostname)
+        self.gateway = self._make_gateway(hostname)
         self.logger = logger or FakeRemoteLogger()
         self.sudo = sudo
+
+    def _make_gateway(self, hostname):
+        if needs_ssh(hostname):
+            return execnet.makegateway('ssh=%s' % hostname)
+        return execnet.makegateway()
 
     def __enter__(self):
         return self
@@ -44,3 +50,16 @@ class FakeRemoteLogger:
 
     def warning(self, *a, **kw):
         pass
+
+
+def needs_ssh(hostname, _socket=None):
+    """
+    Obtains remote hostname of the socket and cuts off the domain part
+    of its FQDN.
+    """
+    _socket = _socket or socket
+    local_hostname = _socket.gethostname()
+    local_short_hostname = local_hostname.split('.')[0]
+    if local_hostname == hostname or local_short_hostname == hostname:
+        return False
+    return True
