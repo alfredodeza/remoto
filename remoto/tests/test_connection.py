19ecc2685c13683541b9c3d08795bcec5c41fa37
Alfredo Deza <alfredo@deza.pe>
2013-10-04 12:09:03 -0400

    add tests for the new ModuleExecution

--- remoto/tests/test_connection.py
+++ remoto/tests/test_connection.py
@@ -20,3 +20,36 @@ class TestNeedsSsh(object):
     def test_hostname_does_not_match(self):
         socket = FakeSocket('foo')
         assert connection.needs_ssh('meh', socket) is True
+
+
+class FakeGateway(object):
+
+    def remote_exec(self, module):
+        pass
+
+
+class TestModuleExecuteArgs(object):
+
+    def setup(self):
+        self.remote_module = connection.ModuleExecute(FakeGateway(), None)
+
+    def test_single_argument(self):
+        assert self.remote_module._convert_args(('foo',)) == "'foo'"
+
+    def test_more_than_one_argument(self):
+        args = ('foo', 'bar', 1)
+        assert self.remote_module._convert_args(args) == "'foo', 'bar', 1"
+
+    def test_dictionary_as_argument(self):
+        args = ({'some key': 1},)
+        assert self.remote_module._convert_args(args) == "{'some key': 1}"
+
+    def test_more_than_one_argument(self):
+        assert self.remote_module._convert_args(('foo', 'bar', 1)) == "'foo', 'bar', 1"
+
+    def test_more_than_one_argument(self):
+        assert self.remote_module._convert_args(('foo', 'bar', 1)) == "'foo', 'bar', 1"
+
+    def test_more_than_one_argument(self):
+        assert self.remote_module._convert_args(('foo', 'bar', 1)) == "'foo', 'bar', 1"
+
