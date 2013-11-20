0ccc9de6c19bac0bf28e86de58a58bb376c9c241
Alfredo Deza <alfredo@deza.pe>
2013-11-20 12:36:02 -0500

    a test for the new remote error class

--- remoto/tests/test_util.py
+++ remoto/tests/test_util.py
@@ -14,3 +14,15 @@ class TestAdminCommand(object):
     def test_command_that_is_not_a_list(self):
         result = util.admin_command(True, 'ls')
         assert result == ['sudo', 'ls']
+
+
+class TestRemoteError(object):
+
+    def test_exception_name(self):
+        traceback = ('\n').join([
+            'Traceback (most recent call last):',
+            '  File "<string>", line 1, in <module>',
+            "NameError: name 'foo' is not defined"
+        ])
+        error = util.RemoteError(traceback)
+        error.exception_name == 'NameError'
