8dfab2edd8b8627b785b727f23f5f1b7d7a1dee7
Alfredo Deza <alfredo@deza.pe>
2014-01-29 12:41:31 -0500

    use new connection defaults for newer execnet

--- remoto/connection.py
+++ remoto/connection.py
@@ -14,10 +14,9 @@ class Connection(object):
         self.logger = logger or FakeRemoteLogger()
         self.remote_module = None
         self.channel = None
-        self.global_timeout = -1  # wait for ever
+        self.global_timeout = None  # wait for ever
         if eager:
             self.gateway = self._make_gateway(hostname)
-            self.gateway.remote_init_threads(threads)
 
     def _make_gateway(self, hostname):
         return execnet.makegateway(
