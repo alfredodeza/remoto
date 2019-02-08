76fad04e1a71658c1682a402cd65501417847d9b
Alfredo Deza <alfredo@deza.pe>
2019-02-08 16:49:50 -0500

    tests add kubernetes connection verifications
    Signed-off-by: Alfredo Deza <alfredo@deza.pe>

---
+++ remoto/tests/backends/test_kubernetes.py
@@ -0,0 +1,36 @@
+from remoto.backends import kubernetes
+
+
+class TestCommandTemplate(object):
+
+    def test_using_podname_only(self):
+        conn = kubernetes.KubernetesConnection('rook-ceph-asdf')
+        tmpl = conn.command_template()
+        assert tmpl == ['kubectl', 'exec', '-i', 'rook-ceph-asdf', '--', '/bin/sh', '-c']
+
+    def test_using_namespace(self):
+        conn = kubernetes.KubernetesConnection('rook-ceph-asdf', 'rook-ceph')
+        tmpl = conn.command_template()
+        assert tmpl == [
+            'kubectl', 'exec', '-i', '-n', 'rook-ceph',
+            'rook-ceph-asdf', '--', '/bin/sh', '-c'
+        ]
+
+
+class TestCommand(object):
+
+    def test_podname_conn_appends(self):
+        conn = kubernetes.KubernetesConnection('rook-ceph-asdf', 'rook-ceph')
+        result = conn.cmd(['ceph', '--version'])
+        assert result == [
+            'kubectl', 'exec', '-i', '-n', 'rook-ceph',
+            'rook-ceph-asdf', '--', '/bin/sh', '-c', 'ceph --version'
+        ]
+
+    def test_namespace_appends(self):
+        conn = kubernetes.KubernetesConnection('rook-ceph-asdf', 'rook-ceph')
+        result = conn.cmd(['ceph', 'health'])
+        assert result == [
+            'kubectl', 'exec', '-i', '-n', 'rook-ceph',
+            'rook-ceph-asdf', '--', '/bin/sh', '-c', 'ceph health'
+        ]
