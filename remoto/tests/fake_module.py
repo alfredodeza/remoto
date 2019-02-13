7859e87695401c9daab7bfcae6120f2296c74021
Alfredo Deza <alfredo@deza.pe>
2019-02-13 13:50:33 -0500

    tests: add more example funcs for remote exec verification
    Signed-off-by: Alfredo Deza <alfredo@deza.pe>

--- remoto/tests/fake_module.py
+++ remoto/tests/fake_module.py
@@ -2,6 +2,24 @@
 this is just a stub module to use to test the `import_module` functionality in
 remoto
 """
+import sys
+
 
 def function(conn):
     return True
+
+
+def fails():
+    raise Exception('failure from fails() function')
+
+
+def unexpected_fail():
+    sys.exit(1)
+
+
+def noop():
+    sys.exit(0)
+
+
+def passes():
+    pass
